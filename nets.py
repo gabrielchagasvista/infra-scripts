ea4213fbe549b9582d8cd392cd8fc8adedf87424
Gabriel Chagas <biel.silva.chagas@gmail.com>
2023-10-03 19:52:54 -0300

    opa

--- nets.py
+++ nets.py
@@ -11,15 +11,14 @@ def crowdstrike_netskope():
     df_crowdstrike_netskope = dfbaseJC_filtered.merge(df_netskope[['Hostname', 'User']], left_on=['hostname'], right_on=['Hostname'], how='left')
     
     df_crowdstrike_netskope['Status Enroll'] = df_crowdstrike_netskope.apply(lambda row: 'Enroll OK' if row['User'] == row['User'] else 'Máquina sem enroll', axis=1)
+    
     df_crowdstrike_netskope['User'] = df_crowdstrike_netskope.apply(lambda row: 'Mesmo do jump' if row['User'] == row.get('email', row['User']) else 'Divergente', axis=1)
     
-    df_crowdstrike_netskope.drop(columns=['Hostname'], inplace=True)  # Remover a coluna 'Hostname'
+    df_crowdstrike_netskope.drop(columns=['Hostname'], inplace=True)  # Remover a coluna Hostname
     
-    # Renomear a coluna 'User' para 'Email Enroll Netskope'
-    df_crowdstrike_netskope.rename(columns={'User': 'Email Enroll Netskope'}, inplace=True)
+    df_crowdstrike_netskope.rename(columns={'User': 'Email Enroll Netskope'}, inplace=True) # renomeia a coluna User
     
-    # Reordenar as colunas no DataFrame resultante
-    columns_order = list(dfbaseJC_filtered.columns) + ['Status Enroll', 'Email Enroll Netskope']
+    columns_order = list(dfbaseJC_filtered.columns) + ['Status Enroll', 'Email Enroll Netskope'] 
     df_result = df_crowdstrike_netskope[columns_order]
     
     df_result.to_csv('potoco.csv', index=False)
