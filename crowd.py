18185d59bfa1435b03aaa42a3eeeb5cda7e574fc
Gabriel Chagas <biel.silva.chagas@gmail.com>
2023-10-03 19:48:51 -0300

    day2

--- crowd.py
+++ crowd.py
@@ -1,27 +1,24 @@
 import pandas as pd
 
+import pandas as pd
 
-def jc_activated(): #filtra apenas os usuários que estão ativos
+def jc_activated(): 
     dfbaseJC = pd.read_csv('base_jc.csv')
     dfbaseJC['username'] = dfbaseJC['username'].astype(str)
     dfbaseJC_filtered = dfbaseJC[dfbaseJC['user_state'] == 'ACTIVATED']
     dfbaseJC_filtered.to_csv('jc_ACTIVATED.csv', index=False)
     return dfbaseJC_filtered
 
-def crowdstrike(dfbaseJC_filtered):
-
+def crowdstrike_netskope():
     df_crowdstrike = pd.read_csv('base_crowdstrike.csv')
-    #dfbase_crowdstrike = pd.read_csv('base_crowdstrike.csv')
+    dfbaseJC_filtered = jc_activated()
     
-    merged_df = df_filtered.merge(df_crowdstrike[['Serial Number']], left_on='serial_number', right_on='Serial Number', how='inner')
+    merged_df = dfbaseJC_filtered.merge(df_crowdstrike[['Serial Number']], left_on='serial_number', right_on='Serial Number', how='inner')
     merged_df['crowdstrike'] = merged_df['Serial Number'].apply(lambda x: 'instalado' if pd.notna(x) else 'nao instalado')
     
-    # Seleciona apenas as colunas 'email', 'hostname' e 'device_os'
     merged_df = merged_df[['email', 'hostname', 'device_os', 'crowdstrike']]
     
-    # Exporta as correspondências para o arquivo 'correspondencias.csv'
     merged_df.to_csv('correspondencias.csv', index=False)
 
-# Chama a função jc_activated() e passa o DataFrame resultante para a função crowdstrike()
-df_filtered = jc_activated()
-crowdstrike(df_filtered)
+
+
