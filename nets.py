56683cd93e81aa64fd7f81af32823c6b0c4e1259
Gabriel Chagas <biel.silva.chagas@gmail.com>
2023-10-07 01:05:56 -0300

    yeah

--- nets.py
+++ nets.py
@@ -1,38 +1,48 @@
 import pandas as pd
 
+
+base_jc = 'csv-prod/base_jc.csv'
+base_netskope = 'csv-prod/base_netskope.csv'
+base_crowdstrike = 'csv-prod/base_crowdstrike.csv'
+
+#base_jc = 'csv-dev/base_jc2.csv'
+#base_netskope = 'csv-dev/base_netskope.csv'
+#base_crowdstrike = 'csv-dev/base_crowdstrike.csv'
+
 def jc_activated(): 
-    dfbaseJC = pd.read_csv('base_jc2.csv')
-    dfbaseJC_filtered = dfbaseJC[dfbaseJC['user_state'] == 'ACTIVATED']
-    dfbaseJC_filtered.to_csv('jc_ACTIVATED.csv', index=False)
+    dfbaseJC = pd.read_csv(base_jc)
+    
+    filter1 = dfbaseJC['user_state'] == 'ACTIVATED'
+    filter2 = ~(dfbaseJC['username'].astype(str).str.startswith('userloftmac.local') | dfbaseJC['username'].astype(str).str.startswith('admgloft'))
+    
+    columns = ['email', 'hostname', 'device_os']
+    dfbaseJC_filtered = dfbaseJC[filter1 & filter2][columns]
+    
     return dfbaseJC_filtered
 
 def netskope():
-    df_netskope = pd.read_csv('base_netskope.csv')
+    df_netskope = pd.read_csv(base_netskope)
     dfbaseJC_filtered = jc_activated()
     
-    # merge netskope
     df_merge_netskope = dfbaseJC_filtered.merge(df_netskope[['Hostname', 'User']], left_on=['hostname'], right_on=['Hostname'], how='left')
+    df_merge_netskope['Status Enroll Netskope'] = df_merge_netskope.apply(lambda row: 
+    'Enroll OK' if (row['hostname'] == row['Hostname'] and row['User'] == row['email']) 
+    else ('Verificar enroll' if (row['hostname'] == row['Hostname'] and row['User'] != row['email'])
+    else 'Máquina sem enroll'), axis=1)
     
-    # Cria coluna 'Status Enroll Netskope' # Valor baseado na correspondência do hostname
-    df_merge_netskope['Status Enroll Netskope'] = df_merge_netskope.apply(lambda row: 'Enroll OK' if row['hostname'] == row['Hostname'] else 'Máquina sem enroll', axis=1)
-    
-    # Criar a coluna 'User' com base na comparação entre 'User' e 'email'. Se forem iguais, define como 'OK', senão mantém o valor original em 'User'
-    df_merge_netskope['Email Enroll Netskope'] = df_merge_netskope.apply(lambda row: 'OK' if row['User'] == row.get('email', row['User']) else row['User'], axis=1)
-    
-    # Remover a coluna 'Hostname' e 'User'
     df_merge_netskope.drop(columns=['Hostname', 'User'], inplace=True)
     
     return df_merge_netskope
 
+
 def crowstrike():
-    df_crowdstrike = pd.read_csv('base_crowdstrike.csv')
+    df_crowdstrike = pd.read_csv(base_crowdstrike)
     dfbaseJC_filtered = jc_activated()
     
-    # Merge com o CrowdStrike
-    df_merge_crowdstrike = dfbaseJC_filtered.merge(df_crowdstrike[['Hostname']], left_on='hostname', right_on='Hostname', how='inner')
-    
-    # Cria coluna 'Status Crowdstrike' # Valor baseado na correspondência do hostname
+    df_merge_crowdstrike = dfbaseJC_filtered.merge(df_crowdstrike[['Hostname']], left_on='hostname', right_on='Hostname', how='left')
     df_merge_crowdstrike['Status Crowdstrike'] = df_merge_crowdstrike['Hostname'].apply(lambda x: 'Instalado' if pd.notna(x) else 'Não instalado')
+
+    df_merge_crowdstrike.drop(columns=['Hostname'], inplace=True)
 
     return df_merge_crowdstrike
 
@@ -40,11 +50,10 @@ def export():
     df_netskope_result = netskope()
     df_crowdstrike_result = crowstrike()
 
-    # Combinar os dois DataFrames usando a chave comum 'hostname'
-    df_combined = df_netskope_result.merge(df_crowdstrike_result[['hostname', 'Status Crowdstrike']], left_on='hostname', right_on='hostname', how='inner')
+    # merge netskope e crowdstrike
+    df_result = df_netskope_result.merge(df_crowdstrike_result, how='outer')
     
-    # Salvar o DataFrame final em um arquivo CSV chamado 'potoco.csv'
-    df_combined.to_csv('potoco.csv', index=False)
+    df_result.to_csv('export.csv', index=False)
 
 
 export()
